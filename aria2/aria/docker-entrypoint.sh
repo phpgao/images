c54b8f2154622034c29cf66c9e843f93dbc27c67
laogao <endoffight@gmail.com>
2019-07-23 10:49:37 +0800

    update aria2

--- aria2/aria/docker-entrypoint.sh
+++ aria2/aria/docker-entrypoint.sh
@@ -7,12 +7,15 @@ chmod +x /done.sh
 
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /etc/aria2/aria2.conf`" ]; then
+    echo adding...
     sed -i '$a bt-tracker='${list} /etc/aria2/aria2.conf
-    echo add......
 else
+    echo updating...
     sed -i "s@bt-tracker.*@bt-tracker=$list@g" /etc/aria2/aria2.conf
-    echo update......
 fi
+echo "done..."
 
+echo "running aria2c"
 usr/bin/aria2c --conf-path=/etc/aria2/aria2.conf -D
+echo "running nginx"
 /usr/sbin/nginx
